faf242087b2ea26a521d9656ce82944d141d99ed
Brett Jackson <brettj85@gmail.com>
2019-04-03 19:47:22 -0500

    mvc from scratch

---
+++ demo 4-3/demo 4-3/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using demo_4_3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demo_4_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Vehicle> Vehicles = new List<Vehicle>()
+        {
+            new Vehicle {color = "red", Model = "Cruze", NumberOfWheels = 4, vin = 123456}
+        };
+        public IActionResult Index()
+        {
+            //ViewBag.Vehicles = Vehicles;
+            return View(Vehicles);
+        }
+    }
+}
---
+++ demo 4-3/demo 4-3/Models/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demo_4_3.Models
+{
+    public class Vehicle
+    {
+        public string Model { get; set; }
+        public int vin { get; set; }
+        public string color { get; set; }
+        public int NumberOfWheels { get; set; }
+    }
+}
